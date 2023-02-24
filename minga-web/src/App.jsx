7751208c258352c7688ab5545c431ed6ac3a05c4
Lucas Matias Santos <lucasmsantoss95@hotmail.com>
2023-02-24 16:04:58 -0300

    diseño register terminado

--- minga-web/src/App.jsx
+++ minga-web/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
-// import Form from "../src/components/Form/Form.jsx";
+import Form from "../src/components/Form/Form.jsx";
 import Header from './Layouts/Header.jsx';
 import SectionMain from './components/Section-main/SectionMain';
 import SectionDown from './components/Section-Down/SectionDown';
+
+
 function App() {
 	
 	
@@ -13,14 +15,14 @@ function App() {
 			<SectionMain/>
 			</div>
 			<SectionDown/>
-			{/* <div className='conteiner'>
+			<div className='conteiner'>
 			<div className='conteiner-form'>
 			<Form/>
-			</div>
+			</div>	
 			<div className='conteiner-img'>
 			<img className="img-form" src="./imgs/formImg.png" alt="Img-Pagina-Form" />
 			</div>
-		</div> */}
+		</div>
 		</body>
 		
 
