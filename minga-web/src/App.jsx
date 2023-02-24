796cd110072eb0f24c367b1de004941f791ad754
Lucas Matias Santos <lucasmsantoss95@hotmail.com>
2023-02-24 11:56:42 -0300

    m-002

--- minga-web/src/App.jsx
+++ minga-web/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+// import Form from "../src/components/Form/Form.jsx";
 import Header from './Layouts/Header.jsx';
 import SectionMain from './components/Section-main/SectionMain';
 import SectionDown from './components/Section-Down/SectionDown';
@@ -12,8 +13,16 @@ function App() {
 			<SectionMain/>
 			</div>
 			<SectionDown/>
-			
+			{/* <div className='conteiner'>
+			<div className='conteiner-form'>
+			<Form/>
+			</div>
+			<div className='conteiner-img'>
+			<img className="img-form" src="./imgs/formImg.png" alt="Img-Pagina-Form" />
+			</div>
+		</div> */}
 		</body>
+		
 
 		
 	)
--- minga-web/src/Layouts/Header.jsx
+++ minga-web/src/Layouts/Header.jsx
@@ -6,7 +6,7 @@ function NavBar(props) {
         <header className='header'>
         <nav className='navbar'>
             <div>
-            <img id="menu" src="./imgs/menu.png" alt="menu" />
+            <img id="menu" src="./imgs/Menu.png" alt="menu" />
                 </div>
                 <div>
                 <img id="logo" src="./imgs/logo.png" alt="logo" />
