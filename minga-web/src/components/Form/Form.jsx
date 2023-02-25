945b7b430749356628f833654f4d56db0fa17dc8
Lucas Matias Santos <lucasmsantoss95@hotmail.com>
2023-02-24 22:41:01 -0300

    form-post

--- minga-web/src/components/Form/Form.jsx
+++ minga-web/src/components/Form/Form.jsx
@@ -2,37 +2,52 @@ import React, { useRef } from "react";
 import "./form.css";
 import Wellcome from "../Wellcome/Wellcome";
 import Image from "../Image/Image";
-import axios from 'axios';
+import axios from "axios";
 
 export default function Form() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const emailNotificationRef = useRef();
 
-    let name = useRef();
-    let email = useRef();
-    let password = useRef();
-    let confirmPassword = useRef();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
+    const data = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      "confirm-password": confirmPasswordRef.current.value,
+      "email-notification": emailNotificationRef.current.checked,
+    };
 
-
-
+    try {
+      const response = await axios.post("http://localhost:8080/users", data);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="register-fondo">
       <div className="register">
         <Wellcome />
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <fieldset>
             <legend>Name</legend>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" ref={nameRef} />
             <Image src="./form-img/profile.svg" />
           </fieldset>
           <fieldset>
             <legend>Email</legend>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" ref={emailRef} />
             <Image src="./form-img/@.svg" />
           </fieldset>
           <fieldset>
             <legend>Password</legend>
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" ref={passwordRef} />
             <Image src="./form-img/lock1.svg" />
           </fieldset>
           <fieldset>
@@ -41,6 +56,7 @@ export default function Form() {
               type="password"
               name="confirm-password"
               id="confirm-password"
+              ref={confirmPasswordRef}
             />
             <Image src="./form-img/lock1.svg" />
           </fieldset>
@@ -49,16 +65,14 @@ export default function Form() {
               type="checkbox"
               name="email-notification"
               id="email-notification"
+              ref={emailNotificationRef}
             />
-            <label htmlFor="email-notification">
-              Send notification to my email
-            </label>
+            <label htmlFor="email-notification">Send notification to my email</label>
           </fieldset>
           <div>
-            {" "}
-            <a href="#" className="sign-up">
+            <button type="submit" className="sign-up">
               Sign up
-            </a>
+            </button>
           </div>
           <a href="#" className="sign-in-google">
             <Image src="./form-img/Google.svg" />
@@ -76,6 +90,7 @@ export default function Form() {
               home page
             </a>
           </p>
+        
         </form>
       </div>
     </div>
