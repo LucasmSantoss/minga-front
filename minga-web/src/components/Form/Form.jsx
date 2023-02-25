da5358a7112adac4ae5fc12ae04c6a77ba56edf0
Lucas Matias Santos <lucasmsantoss95@hotmail.com>
2023-02-24 23:25:18 -0300

    form-postv2

--- minga-web/src/components/Form/Form.jsx
+++ minga-web/src/components/Form/Form.jsx
@@ -9,26 +9,29 @@ export default function Form() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
-  const emailNotificationRef = useRef();
+  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const data = {
-      name: nameRef.current.value,
-      email: emailRef.current.value,
-      password: passwordRef.current.value,
-      "confirm-password": confirmPasswordRef.current.value,
-      "email-notification": emailNotificationRef.current.checked,
-    };
-
-    try {
-      const response = await axios.post("http://localhost:8080/users", data);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
+    let data = {
+        [nameRef.current.name]: nameRef.current.value,
+        [emailRef.current.name]: emailRef.current.value,
+        [passwordRef.current.name]: passwordRef.current.value,
+        [confirmPasswordRef.current.name]: confirmPasswordRef.current.value,
     }
-  };
+    console.log(data)
+    let url = 'http://localhost:8080/users'
+    try {
+        await axios.post(
+            url,    /* URL del endpoint para crear una categoria */
+            data    /* objeto necesario para crear una categoria (tal cual lo armo en postman) */
+        )
+    } catch(error) {
+        console.log(error)
+        console.log('ocurrio un error')
+    }
+}
 
   return (
     <div className="register-fondo">
@@ -65,7 +68,7 @@ export default function Form() {
               type="checkbox"
               name="email-notification"
               id="email-notification"
-              ref={emailNotificationRef}
+              
             />
             <label htmlFor="email-notification">Send notification to my email</label>
           </fieldset>
