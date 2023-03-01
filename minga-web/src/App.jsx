411bcde66eca4ca52dd62144d7d0fd0286c58243
Karen <karen.perez.0395@gmail.com>
2023-02-28 21:53:30 -0300

    inicio maquetado

--- minga-web/src/App.jsx
+++ minga-web/src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css';
-import IndexLayout from './layouts/IndexLayout/IndexLayout';
+import Image from './components/Image/Image';
+// import IndexLayout from './layouts/IndexLayout/IndexLayout';
 // import Footer from './components/Footer/Footer';
 
 function App() {
 
 	return (
+		// <div>
+		// 	<IndexLayout/>
+		// 	{/* <Footer/> */}
+		// </div>
 		<div>
-			<IndexLayout/>
-			{/* <Footer/> */}
+			<div className='fondo-manga'>
+				<h1>Manga</h1>
+			<div className='div-input'>
+  				<input type="search" placeholder="Find your manga here"/>
+				<img className='img' src="./form-img/search.png" alt="" />
+            </div>
+
+			</div>
 		</div>
-		
 	)
 }
 
---
+++ minga-web/src/components/Manga/Manga.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../Navbar/NavBar.jsx';
+
+export default function Manga() {
+  return (
+    <div>
+        <div className='fondo-manga'>
+            <NavBar />
+        </div>
+
+
+
+    </div>
+  )
+}
