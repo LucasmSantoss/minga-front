650f1f47c16e5354e480245e200edc793d79f8b0
IanFeugeas <ianfabricio1889@gmail.com>
2023-03-14 18:54:30 -0300

    Funcionalidad del front terminada

--- minga-web/src/components/MangaUp/MangaUp.jsx
+++ minga-web/src/components/MangaUp/MangaUp.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import "./MangaUp.css"
+import { useRef } from 'react'
+import textActions from '../../store/Search/actions'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 export default function MangaUp() {
+  const search = useRef()
+  const dispatch = useDispatch()
+  const {captureText} = textActions
+
+  function handleSearch(){
+    dispatch(captureText({inputText: search.current[0].value})) 
+  }
+  let text = useSelector(store => store.text.text)
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }
   return (
     <section className='sectionUp'>
             
             <h1 className='h1Mangas'>Mangas</h1>
             <div className='Search'>
               <div className='imgSearch'></div>
-              <form>
-                <input className='inputSearch' type="search" placeholder='Find your manga here'/>
+              <form ref={search} onSubmit={handleSubmit}>
+                <input className='inputSearch' type="text" placeholder='Find your manga here' onChange={handleSearch} defaultValue={text}/>
               </form>
             </div>
     </section>
