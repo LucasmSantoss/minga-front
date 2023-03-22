8432bcea836a2f516af3125bbf8b83f0f1beeba0
IanFeugeas <ianfabricio1889@gmail.com>
2023-03-22 03:04:16 -0300

    Frontend con algunos errores a corregir

---
+++ minga-web/src/components/Chapter/Chapter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './chapter.css'
+import { useSelector } from 'react-redux';
+
+
+export default function Chapter() {
+    let chapter = useSelector(store => store)
+    console.log(chapter)
+
+    return (
+        <div className='chapter'>
+            {chapter.title? <h2>Chapter # {`${chapter.order} - ${chapter.title}`}</h2> : ''}
+            <img src={chapter.cover_photo}/>
+        </div>
+    )
+}
---
+++ minga-web/src/store/Chapters/actions.js
@@ -0,0 +1,63 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const read_chapters = createAsyncThunk(
+    'read_chapters',
+    async ({ id, page, limit }) => {
+        let token = localStorage.getItem('token')
+        let headers = { headers: { 'Authorization': `Bearer ${token}` } }
+        let url = ''
+        if (page) {
+            url = 'http://localhost:8080/api/chapters?manga_id=' + id + '&page=' + page;
+        }
+        if (limit === 0) {
+            url = 'http://localhost:8080/api/chapters?manga_id=' + id + '&limit=' + limit;
+        }
+
+        try {
+            let response = await axios.get(url, headers)
+            console.log(response)
+            return {
+                chapters: response.data.chapter,
+                count: response.data.count
+            }
+        } catch (error) {
+            return {
+                chapters: [],
+                count: 0
+            }
+        }
+    }
+)
+console.log("checkpoint 3")
+console.log(read_chapters)
+
+const get_chapter = createAsyncThunk(
+    'get_chapter',
+    async ({ id }) => {
+        if ( id ) {
+
+            let token = localStorage.getItem('token')
+            let headers = { headers: { 'Authorization': `Bearer ${token}` } }
+            let url = 'http://localhost:8080/api/chapters/' + id;
+            try {
+                let response = await axios.get(url, headers)
+                return {
+                    chapter: response.data.chapter
+                }
+            } catch (error) {
+                return {
+                    chapter: {}
+                }
+            }
+        } else {
+            return {
+                chapter: {}
+            }
+        }
+    }
+)
+
+const actions = { read_chapters, get_chapter }
+
+export default actions
